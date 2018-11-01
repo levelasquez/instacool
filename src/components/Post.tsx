803f8dcdc62fcba0792d63f602197c58e0c53722
Leonardo Velasquez <velasquezleonardo1@gmail.com>
2018-11-01 12:37:47 -0300

    Styling Post component

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -6,13 +6,38 @@ const style = {
   padding: '10px 15px',
 }
 
-export default class Post extends React.Component {
+const footerStyle = {
+  backgroundColor: '#eee',
+  display: 'flex',
+  marginBottom: '-10px',
+  marginLeft: '-15px',
+  width: 'calc(100% + 30px)'
+}
+
+const footerButtonsStyle = {
+  flex: 1,
+  textAlign: 'center',
+} as React.CSSProperties
+
+interface IPostProps {
+  image: string
+}
+
+export default class Post extends React.Component<IPostProps> {
   public render() {
-    const { children } = this.props
+    const { image } = this.props
 
     return (
       <div style={style}>
-        {children}
+        <img src={image} />
+        <div style={footerStyle}>
+          <div style={footerButtonsStyle}>
+            Like
+          </div>
+          <div style={footerButtonsStyle}>
+            Compartir
+          </div>
+        </div>
       </div>
     )
   }
--- src/containers/newsFeed/index.tsx
+++ src/containers/newsFeed/index.tsx
@@ -2,12 +2,16 @@ import * as React from 'react'
 import Container from '../../components/Container'
 import Post from '../../components/Post'
 
+const style = {
+  margin: '0 auto'
+}
+
 export default class NewsFeed extends React.Component {
   public render() {
     return (
       <Container>
-        <div>
-          <Post />
+        <div style={style}>
+          <Post image='http://placekitten.com/300/200' />
         </div>
       </Container>
     )
