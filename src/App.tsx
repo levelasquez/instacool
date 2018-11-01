eb6e4b3032aed4fa704a6f8ad5fc93e7a614401a
Leonardo Velasquez <velasquezleonardo1@gmail.com>
2018-11-01 09:42:58 -0300

    Input component

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import * as React from 'react'
 import './App.css'
 import Card from './components/Card'
 import Container from './components/Container'
+import Input from './components/Input'
 
 class App extends React.Component {
   public render() {
     return (
       <Container>
-        <Card>lala</Card>
+        <Card>
+          <Input placeholder="Correo" label="Correo" />
+          <Input placeholder="Contraseña" label="Contraseña" />
+        </Card>
       </Container>
     );
   }
