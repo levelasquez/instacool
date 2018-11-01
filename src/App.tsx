ca295d5ee98d14e514357f1aa6b7dc2e04843f78
Leonardo Velasquez <velasquezleonardo1@gmail.com>
2018-11-01 12:09:46 -0300

    Config React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,14 @@
 import * as React from 'react'
+import { Route } from 'react-router'
 import './App.css'
-import Button from './components/Button'
-import Card from './components/Card'
-import Center from './components/Center'
-import Container from './components/Container'
-import Input from './components/Input'
-import Link from './components/Link'
-import Title from './components/Title'
+import Login from './containers/auth/Login'
 
 class App extends React.Component {
   public render() {
     return (
-      <Container>
-        <Card>
-          <Title>Iniciar sesión</Title>
-          <Input placeholder="Correo" label="Correo" />
-          <Input placeholder="Contraseña" label="Contraseña" />
-          <Button block={true}>Enviar</Button>
-          <Center>
-            <Link>Ir al registro</Link>
-          </Center>
-        </Card>
-      </Container>
+      <div>
+        <Route exact={true} path='/' component={Login} />
+      </div>
     );
   }
 }
