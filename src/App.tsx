b3fea8bc72ac8c96a128e81bb6d15610e4434551
Leonardo Velasquez <velasquezleonardo1@gmail.com>
2018-11-01 10:03:39 -0300

    Title component

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import Button from './components/Button'
 import Card from './components/Card'
 import Container from './components/Container'
 import Input from './components/Input'
+import Title from './components/Title'
 
 class App extends React.Component {
   public render() {
     return (
       <Container>
         <Card>
+          <Title>Iniciar sesión</Title>
           <Input placeholder="Correo" label="Correo" />
           <Input placeholder="Contraseña" label="Contraseña" />
           <Button>Enviar</Button>
