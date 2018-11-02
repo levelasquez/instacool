9613dadb60a3330d62047560f51a9460f2adb97e
Leonardo Velasquez <velasquezleonardo1@gmail.com>
2018-11-02 12:35:51 -0300

    LoginForm component

--- src/containers/auth/Login.tsx
+++ src/containers/auth/Login.tsx
@@ -1,10 +1,7 @@
 import * as React from 'react'
-import { Link } from 'react-router-dom'
-import Button from '../../components/Button'
 import Card from '../../components/Card'
-import Center from '../../components/Center'
 import Container from '../../components/Container'
-import Input from '../../components/Input'
+import LoginForm from '../../components/LoginForm'
 import Title from '../../components/Title'
 
 export default class Login extends React.Component {
@@ -13,12 +10,7 @@ export default class Login extends React.Component {
       <Container center={true}>
         <Card>
           <Title>Iniciar sesión</Title>
-          <Input placeholder="Correo" label="Correo" />
-          <Input placeholder="Contraseña" label="Contraseña" />
-          <Button block={true}>Enviar</Button>
-          <Center>
-            <Link to='/register'>Ir al registro</Link>
-          </Center>
+          <LoginForm />
         </Card>
       </Container>
     )
--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase'
 
 const config = {
-  apiKey: "AIzaSyCtvcqYgJu1YgxHRhy3fnaqTAQ-KfQSjeM",
-  authDomain: "instacool-f358a.firebaseapp.com",
-  databaseURL: "https://instacool-f358a.firebaseio.com",
-  messagingSenderId: "509762837200",
-  projectId: "instacool-f358a",
-  storageBucket: "instacool-f358a.appspot.com",
+  apiKey: 'AIzaSyCtvcqYgJu1YgxHRhy3fnaqTAQ-KfQSjeM',
+  authDomain: 'instacool-f358a.firebaseapp.com',
+  databaseURL: 'https://instacool-f358a.firebaseio.com',
+  messagingSenderId: '509762837200',
+  projectId: 'instacool-f358a',
+  storageBucket: 'instacool-f358a.appspot.com',
 }
 
 firebase.initializeApp(config)
