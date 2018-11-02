e3b912d1f059a1a7ebacd758d98dd2c878317044
Leonardo Velasquez <velasquezleonardo1@gmail.com>
2018-11-02 10:14:21 -0300

    Config redux and redux-thunk

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,28 @@
 import createHistory from 'history/createBrowserHistory'
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
 import { Router } from 'react-router'
+import { applyMiddleware, combineReducers, createStore,  } from 'redux'
+import thunk from 'redux-thunk'
 import App from './App'
+import * as reducers from './ducks'
 import './index.css'
 import registerServiceWorker from './registerServiceWorker'
+
+const store = createStore(
+  combineReducers({ ...reducers }),
+  applyMiddleware(thunk),
+)
 
 const history = createHistory()
 
 ReactDOM.render(
-  <Router history={history}>
-    <App />
-  </Router>,
+  <Provider store={store}>
+    <Router history={history}>
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById('root') as HTMLElement
 )
 registerServiceWorker()
