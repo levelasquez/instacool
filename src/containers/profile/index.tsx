ccda18f6f78384d91b3ecbd52c1267f9f661f97b
Leonardo Velasquez <velasquezleonardo1@gmail.com>
2018-11-02 09:28:29 -0300

    Styling profile

--- src/containers/profile/index.tsx
+++ src/containers/profile/index.tsx
@@ -1,19 +1,48 @@
 import * as React from 'react'
 import Button from '../../components/Button'
+import Card from '../../components/Card'
 import ProfileImage from '../../components/ProfileImage'
 
 const container = {
+  padding: '15px',
+}
+
+const row = {
   display: 'flex',
   justifyContent: 'space-between',
-  padding: '15px',
+  marginBottom: '10px',
 }
 
 export default class Profile extends React.Component {
   public render() {
     return (
       <div style={container}>
-        <ProfileImage />
-        <Button>Agregar</Button>
+        <div style={row}>
+          <ProfileImage />
+          <Button>Agregar</Button>
+        </div>
+        <div style={row}>
+          <Card>
+            <img src="http://placekitten.com/100/100" />
+          </Card>
+          <Card>
+            <img src="http://placekitten.com/100/100" />
+          </Card>
+          <Card>
+            <img src="http://placekitten.com/100/100" />
+          </Card>
+        </div>
+        <div style={row}>
+          <Card>
+            <img src="http://placekitten.com/100/100" />
+          </Card>
+          <Card>
+            <img src="http://placekitten.com/100/100" />
+          </Card>
+          <Card>
+            <img src="http://placekitten.com/100/100" />
+          </Card>
+        </div>
       </div>
     )
   }
