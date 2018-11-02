e67ab7e89e04e213e1cdb7a89cd1fffc6a3840ff
Leonardo Velasquez <velasquezleonardo1@gmail.com>
2018-11-02 09:15:27 -0300

    Profile container

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import NavBar from './components/NavBar'
 import Login from './containers/auth/Login'
 import Register from './containers/auth/Register'
 import NewsFeed from './containers/newsFeed'
+import Profile from './containers/profile'
 
 class App extends React.Component {
   public render() {
@@ -14,6 +15,7 @@ class App extends React.Component {
         <Route exact={true} path='/register' component={Register} />
         <Route path='/app' component={NavBar} />
         <Route exact={true} path='/app/newsfeed' component={NewsFeed} />
+        <Route exact={true} path='/app/profile' component={Profile} />
       </div>
     );
   }
---
+++ src/containers/profile/index.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react'
+
+const container = {
+  padding: '15px',
+}
+
+export default class Profile extends React.Component {
+  public render() {
+    return (
+      <div style={container}>
+        lala
+      </div>
+    )
+  }
+}
